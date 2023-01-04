1172e315ad8fe64041723e9cdd4f6194cb606a13
rishita1234 <rishita123@gmail.com>
2023-01-04 07:27:54 +0530

    Added: Multiple Rides

--- test/java/com/bridgelabztest/CabInvoiceGeneratorTest.java
+++ test/java/com/bridgelabztest/CabInvoiceGeneratorTest.java
@@ -4,9 +4,8 @@ package com.bridgelabztest;
  * Purpose : To perform JUnit testing on methods of Cab Invoice Generator
  */
 
-import com.bridgelabz5.CabInvoiceGenerator;
-import com.bridgelabz5.InvoiceSummary;
-import com.bridgelabz5.Ride;
+import com.bridgelabz2.CabInvoiceGenerator;
+import com.bridgelabz2.Ride;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -44,52 +43,5 @@ public class CabInvoiceGeneratorTest {
         double totalFare = inVoiceGenerator.calculateFare(distance, time);
 
         Assert.assertEquals(5.0, totalFare, 0.0);
-    }
-
-    /**
-     * Test Case 3 : To get total fare given multiple rides
-     */
-    @Test
-    public void givenMultipleRides_ShouldReturnInVoiceSummary() {
-        Ride[] rides = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1)};
-        InvoiceSummary summary = inVoiceGenerator.calculateFare(rides);
-
-        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
-
-        Assert.assertEquals(expectedInvoiceSummary, summary);
-    }
-
-    /**
-     * Test Case 4 : To get list of rides from ride repository when user id is given
-     */
-    @Test
-    public void givenUserID_ShouldGetTheListOfRidesFromRepo_ReturnInVoice() {
-        int userID = 1;
-        Ride[] rides = {
-                new Ride(2.0, 5),
-                new Ride(0.1, 1)};
-        InvoiceSummary summary = inVoiceGenerator.calculateFare(rides);
-
-        Hashtable<Integer,Ride[]> listOfRides = new Hashtable<>();
-
-        listOfRides.put(userID,rides);
-
-        InvoiceSummary expectedInVoice = inVoiceGenerator.inVoiceService(listOfRides);
-        Assert.assertEquals(expectedInVoice,summary);
-    }
-
-    /**
-     * Test Case 5 : TO get premium rides cost
-     */
-    @Test
-    public void ifSelectedPremiumRides_ShouldReturnPremiumPrices() {
-
-        double distance = 4.0;
-        int time = 10;
-        double totalFare = inVoiceGenerator.calculateFare(distance, time);
-
-        Assert.assertEquals(80.0, totalFare, 0.0);
     }
 }
